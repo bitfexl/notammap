f64ddab491dac6f0f63c2f2360dfc2816c397142
bitfexl <54662051+bitfexl@users.noreply.github.com>
2023-07-21 14:44:26 +0200

    added tailwindcss, project config

--- notammap/src/App.tsx
+++ notammap/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Notam Map</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <p>{import.meta.env.VITE_MY_VAR}</p>
+                <div className="inline-block w-40 h-20 border-red-500 border-8"></div>
+                <br />
+                <div className="inline-block w-40 h-20 bg-green-500"></div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
