6f1f4d6ed85ba14ddc799434b5a2cffe0375d670
bitfexl <54662051+bitfexl@users.noreply.github.com>
2025-03-16 14:47:34 +0100

    style fixes

--- notammap/src/lib/form/Help.tsx
+++ notammap/src/lib/form/Help.tsx
@@ -10,12 +10,12 @@ export function Help({ children }: any) {
 
     return (
         <span className="relative z-[99999] opacity-100" onPointerEnter={() => setShowHelp(true)} onPointerLeave={() => setShowHelp(false)}>
-            (?)
+            <span className="select-none">(?)</span>
             {showHelp ? (
                 <div className="absolute top-1 pb-4 left-1/2 -translate-x-1/2 -translate-y-full">
-                    <div className="relative bg-white shadow-lg rounded-sm border border-gray-300 p-2 w-max">
-                        {children}
+                    <div className="relative bg-white shadow-lg rounded-sm border border-gray-300 w-max">
                         <div className="w-4 h-4 absolute -bottom-2 left-1/2 -translate-x-1/2 rotate-45 bg-white border-gray-300 border-r border-b"></div>
+                        <div className="p-2 relative">{children}</div>
                     </div>
                 </div>
             ) : null}
