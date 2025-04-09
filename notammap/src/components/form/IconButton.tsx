123eb91f005a88c39a540c2dc6aae8dc4a006709
bitfexl <54662051+bitfexl@users.noreply.github.com>
2025-04-09 22:02:21 +0200

    fixed shadow when connected

--- notammap/src/components/form/IconButton.tsx
+++ notammap/src/components/form/IconButton.tsx
@@ -7,42 +7,42 @@ export interface IconButtonProps {
     connected?: "bottom" | "bottom-right" | "bottom-left" | "none";
 }
 
-// TODO: use this button also for map layer selection
-
 export function IconButton({ svgIcon, onClick, connected = "none" }: IconButtonProps) {
     return (
-        <button
-            className={`nostyle inline-block rounded-t-md ${
-                !connected.startsWith("bottom") && "rounded-b-md"
-            } bg-white w-10 h-10 cursor-pointer z-10`}
-            style={boxShadowStyle}
-            onClick={onClick}
-        >
-            <div className="p-2">
-                <SVGIcon svg={svgIcon}></SVGIcon>
-            </div>
-            <div className={`${connected != "none" && "pb-2"} bg-white`}></div>
-            {connected.startsWith("bottom") && (
-                <div
-                    className="w-full h-[6px] bg-white"
-                    style={
-                        connected != "bottom-left"
-                            ? {
-                                  boxShadow: "5px 0 0px white",
-                              }
-                            : {}
-                    }
-                >
-                    {connected != "bottom-right" && (
-                        <div
-                            className="h-full"
-                            style={{
-                                boxShadow: "-5px 0 0px white",
-                            }}
-                        ></div>
-                    )}
+        <div className="h-10 z-10">
+            <button
+                className={`nostyle inline-block rounded-t-md ${!connected.startsWith("bottom") && "rounded-b-md"} bg-white w-10 ${
+                    connected == "none" ? "h-10" : "h-12"
+                } cursor-pointer`}
+                style={boxShadowStyle}
+                onClick={onClick}
+            >
+                <div className="p-2">
+                    <SVGIcon svg={svgIcon}></SVGIcon>
                 </div>
-            )}
-        </button>
+                <div className="pb-2"></div>
+                {connected.startsWith("bottom") && (
+                    <div
+                        className="w-full h-[6px] bg-white"
+                        style={
+                            connected != "bottom-left"
+                                ? {
+                                      boxShadow: "5px 0 0px white",
+                                  }
+                                : {}
+                        }
+                    >
+                        {connected != "bottom-right" && (
+                            <div
+                                className="h-full"
+                                style={{
+                                    boxShadow: "-5px 0 0px white",
+                                }}
+                            ></div>
+                        )}
+                    </div>
+                )}
+            </button>
+        </div>
     );
 }
