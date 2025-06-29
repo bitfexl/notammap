88a6464c49c2c1f7497c329146da8c9f3b6d0a72
bitfexl <54662051+bitfexl@users.noreply.github.com>
2025-06-29 21:54:38 +0200

    added q code filter

---
+++ notammap/src/components/menu/filter/QCodeFilter.tsx
@@ -0,0 +1,175 @@
+import categoriesCollections from "../../../assets/QCodeCategoriesCollections.json";
+import categories from "../../../assets/QCodeCategories.json";
+import { firstTablesTable } from "../../../assets/QCodeTables.json";
+import { useState } from "react";
+
+interface Category {
+    name: string;
+    codes: string[];
+}
+
+const categoriesCollectionWithCategories = categoriesCollections.map((c) => ({ ...c, categories: ((): Category[] => [])() }));
+const otherCategories: Category[] = [];
+
+for (const category of categories) {
+    let added = false;
+    for (const categoryCollection of categoriesCollectionWithCategories) {
+        if (category.name.startsWith(categoryCollection.id + " ")) {
+            categoryCollection.categories.push({ ...category, name: category.name.substring(categoryCollection.id.length + 1) });
+            added = true;
+            break;
+        }
+    }
+    if (!added) {
+        otherCategories.push(category);
+    }
+}
+
+export interface QCodeFilterProps {}
+
+export function QCodeFilter({}: QCodeFilterProps) {
+    const [selectedCodes, setSelectedCodes] = useState<string[]>([]);
+
+    function updateCodes(codes: string[], selected: boolean) {
+        let newCodes;
+        if (selected) {
+            newCodes = [...selectedCodes];
+            for (const code of codes) {
+                if (!newCodes.includes(code)) {
+                    newCodes.push(code);
+                }
+            }
+        } else {
+            newCodes = selectedCodes.filter((c) => !codes.includes(c));
+        }
+        setSelectedCodes(newCodes);
+    }
+
+    return (
+        <div className="flex flex-col gap-4">
+            {categoriesCollectionWithCategories.map((cc) => (
+                <CategoryCollectionComponent
+                    name={cc.name}
+                    categories={cc.categories}
+                    selectedCodes={selectedCodes}
+                    updateCodes={updateCodes}
+                    key={cc.id}
+                ></CategoryCollectionComponent>
+            ))}
+            {otherCategories.map((c) => (
+                <CategoryComponent category={c} selectedCodes={selectedCodes} updateCodes={updateCodes} key={c.name}></CategoryComponent>
+            ))}
+        </div>
+    );
+}
+
+function CategoryCollectionComponent({
+    name,
+    categories,
+    selectedCodes,
+    updateCodes,
+}: {
+    name: string;
+    categories: Category[];
+    selectedCodes: string[];
+    updateCodes: (codes: string[], selected: boolean) => void;
+}) {
+    const codesInCategoryCollection = categories.flatMap((c) => c.codes);
+    const selectedCodesInCategoryCollection = selectedCodes.filter((c) => codesInCategoryCollection.includes(c));
+
+    return (
+        <div>
+            <details>
+                <summary className="cursor-pointer">
+                    <span>
+                        <Checkbox
+                            onChange={(checked) => updateCodes(codesInCategoryCollection, checked)}
+                            state={
+                                selectedCodesInCategoryCollection.length == 0
+                                    ? "none"
+                                    : selectedCodesInCategoryCollection.length == codesInCategoryCollection.length
+                                    ? "all"
+                                    : "partial"
+                            }
+                        ></Checkbox>{" "}
+                        <b>{name}</b>
+                    </span>
+                </summary>
+                <div className="flex flex-col gap-2 pt-2">
+                    {categories.map((c) => (
+                        <div className="pl-4">
+                            <CategoryComponent
+                                category={c}
+                                selectedCodes={selectedCodes}
+                                updateCodes={updateCodes}
+                                key={c.name}
+                            ></CategoryComponent>
+                        </div>
+                    ))}
+                </div>
+            </details>
+        </div>
+    );
+}
+
+function CategoryComponent({
+    category,
+    selectedCodes,
+    updateCodes,
+}: {
+    category: Category;
+    selectedCodes: string[];
+    updateCodes: (codes: string[], selected: boolean) => void;
+}) {
+    const codesInCategory = category.codes;
+    const selectedCodesInCategory = selectedCodes.filter((c) => codesInCategory.includes(c));
+
+    return (
+        <div>
+            <details>
+                <summary className="cursor-pointer">
+                    <span>
+                        <Checkbox
+                            onChange={(checked) => updateCodes(codesInCategory, checked)}
+                            state={
+                                selectedCodesInCategory.length == 0
+                                    ? "none"
+                                    : selectedCodesInCategory.length == codesInCategory.length
+                                    ? "all"
+                                    : "partial"
+                            }
+                        ></Checkbox>{" "}
+                        <b>{category.name}</b>
+                    </span>
+                </summary>
+                <ul className="pl-4">
+                    {category.codes.map((c) => (
+                        <li key={c}>
+                            <label>
+                                <input
+                                    onChange={(e) => updateCodes([c], e.target.checked)}
+                                    checked={selectedCodesInCategory.includes(c)}
+                                    type="checkbox"
+                                />{" "}
+                                (<code>{c}</code>) {(firstTablesTable as any)[c]}
+                            </label>
+                        </li>
+                    ))}
+                </ul>
+            </details>
+        </div>
+    );
+}
+
+function Checkbox({ state, onChange }: { state: "all" | "none" | "partial"; onChange: (checked: boolean) => void }) {
+    return (
+        <input
+            onChange={(e) => onChange(e.target.checked)}
+            checked={state == "all"}
+            ref={(r) => {
+                if (r) r.indeterminate = state == "partial";
+            }}
+            type="checkbox"
+        />
+    );
+}
