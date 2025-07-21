14e646641120722df7fa1d09a33e3b70522e72dc
bitfexl <54662051+bitfexl@users.noreply.github.com>
2025-07-21 20:31:41 +0200

    added update script

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# update sources
+oldhash=$(git rev-parse HEAD)
+git pull
+newhash=$(git rev-parse HEAD)
+
+# check for updates
+if [ "$oldhash" = "$newhash" ]
+then
+    echo "No update, staying on $oldhash"
+else
+    # save previous logs
+    mkdir -p logs
+    podman compose ps -f '{{.Names}}' | (
+        while read name
+        do
+            filename=$(date -I)-$name.log
+            podman compose logs -tn 2>&1 | grep --color=never $name > logs/$filename
+            echo "saved logs/$filename"
+        done
+    )
+
+    # rebuild and restart
+    podman compose build --pull --no-cache
+    podman compose down
+    podman compose up -d
+
+    echo "Updated from $oldhash to $newhash"
+fi
