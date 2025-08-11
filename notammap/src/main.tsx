72b3c08b264b4ad9396433af93823b4e4c92fcb6
bitfexl <54662051+bitfexl@users.noreply.github.com>
2025-08-11 19:31:25 +0200

    removed unused code

--- notammap/src/main.tsx
+++ notammap/src/main.tsx
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./components/app/App.tsx";
 import "./css/index.css";
 import { ErrorBoundary } from "./components/util/ErrorBoundary.tsx";
-import { LeafletMapCustomRendererTest } from "./components/map/customRendererTest/LeafletMapCustomRendererTest.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <ErrorBoundary>
             <App />
-            {/* <LeafletMapCustomRendererTest></LeafletMapCustomRendererTest> */}
         </ErrorBoundary>
     </React.StrictMode>
 );
