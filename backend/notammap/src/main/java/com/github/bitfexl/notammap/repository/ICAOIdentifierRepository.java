2d138e67f5de4e50ba76897e51553ecec46f2dde
bitfexl <54662051+bitfexl@users.noreply.github.com>
2026-06-07 23:00:50 +0200

    added fir query

--- backend/notammap/src/main/java/com/github/bitfexl/notammap/repository/ICAOIdentifierRepository.java
+++ backend/notammap/src/main/java/com/github/bitfexl/notammap/repository/ICAOIdentifierRepository.java
@@ -1,9 +1,20 @@
 package com.github.bitfexl.notammap.repository;
 
 import com.github.bitfexl.notammap.repository.entities.ICAOIdentifier;
+import com.github.bitfexl.notammap.repository.entities.types.IdentifierType;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.List;
+
 @ApplicationScoped
 public class ICAOIdentifierRepository implements PanacheRepositoryBase<ICAOIdentifier, String> {
+    @SuppressWarnings("unchecked")
+    public List<String> getFIRIdentifiers(List<String> identifiers) {
+        return getEntityManager()
+                .createQuery("SELECT id FROM ICAOIdentifier WHERE type = :type AND id IN :identifiers")
+                .setParameter("type", IdentifierType.FIR)
+                .setParameter("identifiers", identifiers)
+                .getResultList();
+    }
 }
